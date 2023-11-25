894370310dbc35423de45824bbb23783b7a640b6
Orxan <ibrahimzada003@gmail.com>
2023-11-25 10:32:37 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Text.RegularExpressions;
+string pattern = @"^\w[\w-]*@[\w]+(\.[\w]+)+";
+Console.WriteLine(Regex.IsMatch("uiorxan@gmail.com",pattern));
+Console.WriteLine(Regex.IsMatch("uiorxan@code.edu.az", pattern));
+Console.WriteLine(Regex.IsMatch("ui-orxan@code.edu.az", pattern));
+Console.WriteLine(Regex.IsMatch("_uiorxan@code.edu.az", pattern));
